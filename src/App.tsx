a0ad445d0d3cb261a78b82b713b3535c99369bf8
Sarah L <slaukilo@gmail.com>
2022-11-18 13:04:13 -0700

    Problem Solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useLayoutEffect} from "react";
+
+
+export const createRandomHex = () => {
+  let ranHex = Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
+  return `#${ranHex.toUpperCase()}`
+};
+
+// console.log(createRandomHex());
+
+export const createHexArr = () => {
+  var arr = [];
+  for ( let i = 0; i<3; i++) {
+      let tmp = createRandomHex();
+      arr.push(tmp);
+  }
+  // debugger;
+  return arr;
+};
+
+export const randomHex = (arr: any) => {
+  return arr[Math.floor(Math.random() * arr.length)]
+};
+
+var hexArr = createHexArr();
+// console.log(hexArr)
+
+enum Result {
+  Correct,
+  Incorrect
+};
 
 function App() {
-  return (
+  const [color, setColor] = useState("");
+  const [answer, setAnswer] = useState<string[]>([]);
+  const [result, setResult] = useState<Result | undefined>(undefined);
+
+  const createColors = () => {
+    const realColor = createRandomHex();
+    setColor(realColor);
+    setAnswer([realColor, createRandomHex(), createRandomHex()].sort((
+    ) => 0.5 - Math.random()));
+  }
+
+  useEffect(() => {
+    createColors();
+  }, []);
+
+  const answerHandlingClicked = (answer: string) => {
+    if (answer === color) {
+      setResult(Result.Correct);
+      createColors();
+    } else {
+      setResult(Result.Incorrect);
+    }
+  }
+  
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className='guess-color' style={{ background: color }}></div>        
+        {answer.map((answer) => (
+          <button onClick={() => answerHandlingClicked(answer)} key={answer}>
+            {answer}
+          </button>
+        ))}
+        
+        {result === Result.Incorrect && <div className='incorrect'>Incorrect Answer!</div>}
+        {result === Result.Correct && <div className='correct'>Correct Answer!</div>}  
+      </div>
     </div>
   );
 }
